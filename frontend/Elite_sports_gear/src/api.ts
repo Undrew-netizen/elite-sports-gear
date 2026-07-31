5abb2ef9e2e25f7218d0ec543ff07e2b02898cca
undrew-netizen <andrewarnolddrew@gmail.com>
2026-07-31 15:28:49 +0300

    added backend url

--- frontend/Elite_sports_gear/src/api.ts
+++ frontend/Elite_sports_gear/src/api.ts
@@ -1,3 +1,3 @@
-const defaultApiBaseUrl = 'http://127.0.0.1:8000'
+const defaultApiBaseUrl = 'https://elitesportsgear-co-ke.onrender.com'
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || defaultApiBaseUrl
